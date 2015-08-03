f7bd8f477e95b2be64dcafc6d6a9b45dc9b81221
cb1234 <chula_bandara@hotmail.com>
2015-08-03 16:14:53 -0700

    adding more week1 files

--- read_yaml.py
+++ read_yaml.py
@@ -2,13 +2,13 @@ import json
 import yaml
 import pprint
 
-with open("some_file.yml") as f:
+with open("my_test.yml") as f:
     new_list = yaml.load(f)
 pprint.pprint(new_list)
 
 
 
-with open("my_file.json") as f:
+with open("my_test.json") as f:
     new_list1 = json.load(f)
 pprint.pprint(new_list1)
 
