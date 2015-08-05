2b493b894b3a88e3d4e135d3d4418dc2ca062214
cb1234 <chula_bandara@hotmail.com>
2015-08-04 18:26:41 -0700

    adding learnpy class3 ex1

--- learnpy/class3/ex1_binary_converter.py
+++ learnpy/class3/ex1_binary_converter.py
@@ -2,61 +2,74 @@
 '''
 Disclaimer - This is a solution to the below problem given the content we have
 discussed in class.  It is not necessarily the best solution to the problem.
-In other words, I only use things we have covered up to this point in the class.
+In other words, I only use things we have covered up to this point in the class
+(with some minor exceptions).
 Python for Network Engineers
 https://pynet.twb-tech.com
 Learning Python
-Class#2
-2. Create an IP address converter (dotted decimal to binary):
-    A. Prompt a user for an IP address in dotted decimal format.
-    B. Convert this IP address to binary and display the binary result on the
-screen (a binary string for each octet).
-    Example output:
-    first_octet    second_octet     third_octet    fourth_octet
-         0b1010       0b1011000          0b1010         0b10011
+Class#3
+I. Create an IP address converter (dotted decimal to binary).  This will be
+similar to what we did in class2 except:
+    A. Make the IP address a command-line argument instead of prompting the user
+for it.
+            ./binary_converter.py 10.88.17.23
+    B. Simplify the script logic by using the flow-control statements that we
+learned in this class.
+    C. Zero-pad the digits such that the binary output is always 8-binary digits
+long.  Strip off the leading '0b' characters.  For example,
+        OLD:     0b1010
+        NEW:    00001010
+    D. Print to standard output using a dotted binary format.  For example,
+        IP address          Binary
+        10.88.17.23        00001010.01011000.00010001.00010111
+    Note, you will probably need to use a 'while' loop and a 'break' statement
+for part C.
+        while True:
+            ...
+            break       # on some condition (exit the while loop)
+    Python will execute this loop again and again until the 'break' is encountered.
 '''
 
-#network = raw_input("\n\nEnter an IP address: ")
 import sys
 
 if len(sys.argv) != 2:
-   # Exist the script
-  sys.exit("Usage: ./ex1_binary_converter.py <ip_address>")   
+    # Exit the script
+    sys.exit("Usage: ./ex1_binary_converter.py <ip_address>")
 
 
 ip_addr = sys.argv.pop()
-
 octets = ip_addr.split(".")
 
+# create a blank list (needed because I use .append() method below)
 ip_addr_bin = []
 
 if len(octets) == 4:
-    
-    for octet in octets:
-        bin_octet = bin(str(octet))
 
-        while True:
-            if len(bin_octet) >= 8:
-                break
+    for octet in octets:
+
+        bin_octet = bin(int(octet))
+
+        # strip off '0b' from front of string (you can slice a string also)
+        bin_octet = bin_octet[2:]
+        if len(bin_octet) > 8:
+            sys.exit("Invalid Octet")
+        # prepend '0' to number until 8 chars long
+ 
+        while len(bin_octet) < 8:
+            
             bin_octet = '0' + bin_octet
-    
+
+        # add octet to new list
         ip_addr_bin.append(bin_octet)
 
-     ip_addr_bin = ".".join(ip_addr_bin)
 
-     print "\n%-15s %-45s" % ("IP address", "Binary")
-     print "\n%-15s %-45s" % ("IP address", "Binary")
+    # join binary number in dotted-binary format
+    ip_addr_bin = ".".join(ip_addr_bin)
+
+    # print the output
+    print "\n%-15s %-45s" % ("IP address", "Binary")
+    print "%-15s %-45s\n\n" % (ip_addr, ip_addr_bin)
+
 else:
-     sys.exit("Invalid IP address entered")
+    sys.exit("Invalid IP address entered")
 
-
-
-
-#first_octet_bin = bin(int(octets[0]))
-#second_octet_bin = bin(int(octets[1]))
-#third_octet_bin = bin(int(octets[2]))
-#fourth_octet_bin = bin(int(octets[3]))
-
-#print "\n\n%15s %15s %15s %15s" % ("first_octet", "second_octet", "third_octet", "fourth_octet")
-3print "%15s %15s %15s %15s\n" % (first_octet_bin, second_octet_bin,
-                                 third_octet_bin, fourth_octet_bin)
